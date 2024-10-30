c76026d6a96de8028d25beaef72c3c64ae785112
Juri-Wist <juriwist92@gmail.com>
2024-10-30 23:22:04 +0100

    update date format (regex)

--- src/test/DailyImplTest.java
+++ src/test/DailyImplTest.java
@@ -31,4 +31,39 @@ class DailyImplTest {
         trainingDiary.deleteTraining(1);
         assertEquals(0, trainingDiary.getAllTrainings().size());
     }
+
+
+    // добавил два теста
+
+
+    // корректно обновляет существующую запись
+    @Test
+    void updateTraining() {
+        Training training = new Training(1, "running", LocalDate.now(), 5.0, 0.5);
+        trainingDiary.addTraining(training);
+
+        Training updatedTraining = new Training(1, "walking", LocalDate.now().plusDays(1), 10.0, 1.0);
+        trainingDiary.updateTraining(1, updatedTraining);
+
+        Training result = trainingDiary.getTrainingById(1);
+        assertEquals("walking", result.getType());
+        assertEquals(10.0, result.getDistance());
+        assertEquals(1.0, result.getDuration());
+    }
+
+    // возвращает нужный объект, если он существует, и null, если объект с таким id не найден
+    @Test
+    void getTrainingById() {
+        Training training = new Training(1, "running", LocalDate.now(), 5.0, 0.5);
+        trainingDiary.addTraining(training);
+
+        Training result = trainingDiary.getTrainingById(1);
+        assertNotNull(result);
+        assertEquals(training, result);
+
+        Training notFound = trainingDiary.getTrainingById(2);
+        assertNull(notFound);
+    }
+
+
 }
